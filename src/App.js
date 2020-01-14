0896d411b3c9f5fbe6068c964e630952c3ad4060
Jonathan <jonathanbho@outlook.com>
2020-01-14 11:50:02 -0500

    started implementing the mobile drawer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Drawer from 'rc-drawer'
 import logo from './logo.svg';
 import './App.css';
 import NavBar from "./NavBar";
@@ -8,18 +9,50 @@ import ToolBox from "./ToolBox";
 import WorkExperience from "./WorkExperience";
 import SiteDetails from './SiteDetails.js'
 
+export default class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar/>
-      <NamePlate/>
-      <AboutMeView/>
-      <ToolBox/>
-      <WorkExperience/>
-      <SiteDetails/>
-    </div>
-  );
+    constructor(props) {
+        super(props);
+        this.state = {
+            width: window.innerWidth
+        }
+    }
+    componentWillMount() {
+        window.addEventListener('resize', this.handleWindowResize)
+    }
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowSizeChange);
+    }
+    handleWindowSizeChange = () => {
+        this.setState({ width: window.innerWidth });
+        this.forceUpdate();
+    };
+
+    render () {
+        const { width } = this.state;
+        const isMobile = width <= 500;
+        if (isMobile) {
+            return (<div className="App">
+                <NamePlate/>
+                <AboutMeView/>
+                <ToolBox/>
+                <WorkExperience/>
+                <SiteDetails/>
+            </div>
+            );
+        } else {
+            return (
+                <div className="App">
+                    <NavBar/>
+                    <NamePlate/>
+                    <AboutMeView/>
+                    <ToolBox/>
+                    <WorkExperience/>
+                    <SiteDetails/>
+                </div>
+            );
+        }
+
+    }
 }
 
-export default App;
